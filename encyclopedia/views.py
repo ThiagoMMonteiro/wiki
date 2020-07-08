0f9f49fe746f9503e913c07d742851b7a0858755
Thiago Mânica Monteiro <thiago.monteiro2608@gmail.com>
2020-07-08 12:14:00 -0300

    Adding a start project and create entry page

---
+++ encyclopedia/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, entry):
+    if util.get_entry(entry):
+        return render(request, "encyclopedia/entry.html", {
+            "entry": util.get_entry(entry),
+            "entry_title": entry.capitalize()
+        })
+
